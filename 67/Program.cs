97edef24b5882f3d4680b20624eea788ba9ba31a
geekbrains <info@geekbrains.ru>
2022-12-10 18:37:49 +0500

    task67

---
+++ 67/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.WriteLine(GetSum(45));
+int GetSum (int num)
+{
+    if (num == 0 )
+    {
+        return 0;
+    }
+    return (num % 10 + GetSum(num / 10));
+}
+
