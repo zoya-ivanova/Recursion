acfbe73de4a1bcba11ce8ceed980cad1dc11045f
geekbrains <info@geekbrains.ru>
2022-12-10 18:39:02 +0500

    task63

---
+++ 63/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 63: Задайте значениe N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// Задача: Задайте значения от М до N. Напишите программу, которая выведет все натуральные числа в промежутке от М до N.
+
+Console.WriteLine(PrintNumbers(1, 10));
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return (start + " " + PrintNumbers(++start, end));
+}
