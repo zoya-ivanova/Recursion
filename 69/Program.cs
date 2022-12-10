6198689e9e1d35a3c5ad8ae09805cf03d2b4c68f
geekbrains <info@geekbrains.ru>
2022-12-10 18:35:53 +0500

    task69

---
+++ 69/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 69 Напишите программу, которая на вход принимает два числа А и В и 
+// возводит число А в целую степерь В с помощью рекурсии.
+
+System.Console.WriteLine(Pow(2, 10));
+int Pow(int num, int rang)
+{
+    if (rang == 0)
+    {
+        return 1;
+    }
+    if (rang == 1)
+    {
+        return num;
+    }
+    return (num * Pow(num, --rang));
+}
