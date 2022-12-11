a901ad1ab85402ed2caa8e6e9e2b94c0b6f758e7
geekbrains <info@geekbrains.ru>
2022-12-11 12:56:24 +0500

    task66

---
+++ 66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SummaElements(m, n));
+
+int SummaElements(int start, int end)
+{
+    if (start == end) 
+    {
+        return start;
+    }
+    if (start < end) 
+    {
+        return end + SummaElements(start, end - 1);
+    }
+    return end + SummaElements(start, end + 1);
+}
--- 69/Program.cs
+++ 69/Program.cs
@@ -1,16 +1,27 @@
 ﻿// Задача 69 Напишите программу, которая на вход принимает два числа А и В и 
 // возводит число А в целую степерь В с помощью рекурсии.
 
-System.Console.WriteLine(Pow(2, 10));
-int Pow(int num, int rang)
+// System.Console.WriteLine(Pow(2, 10));
+// int Pow(int num, int rang)
+// {
+//     if (rang == 0)
+//     {
+//         return 1;
+//     }
+//     if (rang == 1)
+//     {
+//         return num;
+//     }
+//     return (num * Pow(num, --rang));
+// }
+
+
+Console.WriteLine(Sum(1, 10));
+int Sum(int start, int end)
 {
-    if (rang == 0)
+    if (end == 0)
     {
-        return 1;
+        return start;
     }
-    if (rang == 1)
-    {
-        return num;
-    }
-    return (num * Pow(num, --rang));
+    return (end + Sum(start + 1, end));
 }
